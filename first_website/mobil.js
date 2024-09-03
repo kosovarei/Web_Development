0a6df33c73e5fcbd2806f097f45a7ef4c509ed30
kosovarei <111733299+kosovarei@users.noreply.github.com>
2024-09-03 15:13:20 +0200

    oranizing files

---
+++ first_website/mobil.js
@@ -0,0 +1,12 @@
+const menu = document.getElementById("nav-menu");
+
+
+const hamMenu = () =>{
+    if(menu.style.display == "block"){
+        menu.style.display = "none";
+
+    }
+    else{
+        menu.style.display = "block";
+    }
+}
